666c49d0fc6ca51f6c31ca707674db67a253cddc
Alex <iambethharmon@protonmail.com>
2023-03-05 04:05:57 -0700

    Metamask Snap integration with Push Protocol

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,5 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import { divider, heading, panel, text } from '@metamask/snaps-ui';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -11,21 +11,38 @@ import { panel, text } from '@metamask/snaps-ui';
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
  */
+
 export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+  const fetchNotifs = async () => {
+    const address = await ethereum.request({ method: 'eth_requestAccounts' });
+    const resp = await fetch(
+      `https://backend-staging.epns.io/apis/v1/users/eip155:42:${address}/feeds?spam=true`,
+    );
+
+    return await resp.json();
+  };
+
   switch (request.method) {
     case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'Confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
+      return fetchNotifs().then((resp) => {
+        /* Create a header and body for each notification */
+        const sections = [];
+        sections.push(heading('--- Notifications ---'));
+        sections.push(divider());
+        for (const feed of resp.feeds) {
+          sections.push(heading(feed.payload.notification.title));
+          sections.push(text(feed.payload.notification.body));
+          sections.push(divider());
+        }
+
+        /* Display the panel containing all the notifications. */
+        snap.request({
+          method: 'snap_dialog',
+          params: {
+            type: 'Alert',
+            content: panel(sections),
+          },
+        });
       });
     default:
       throw new Error('Method not found.');
